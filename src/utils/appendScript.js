d56cde0760919905e5744b3fcff8fc297e474c79
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2023-06-28 23:43:52 -0700

    optimize

--- src/utils/appendScript.js
+++ src/utils/appendScript.js
@@ -2,7 +2,6 @@ export const appendScript = (id, scriptToAppend,) => {
     const script = document.createElement("script");
     script.src = scriptToAppend;
     script.id = id;
-    
-    //script.async = true;
+    script.async = true;
     document.body.appendChild(script);
 };
